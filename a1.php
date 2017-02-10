0c78e4807e02fab5ec996780441d6f4592ff37f4
Jihaan Karjeker <jihaank@yahoo.com>
2017-02-09 21:02:18 -0600

    Update README file; added index file with an image.

---
+++ a1.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="UTF-8" />
+        <title>Week 6 - Images</title>
+        <style type="text/css">
+        body {
+            font-family: verdana, arial, helvetica, sans-serif;
+            margin-left: 10%;
+            margin-right: 10%;
+            text-align: center 
+            }
+            h1 {color: hotpink}
+            p {color: rebeccapurple}
+            
+       </style>
+    </head>
+
+    <body>
+    
+    <h1>Jihaan Karjeker</h1>
+    <img src='jihaan.jpg' alt='Jihaan'>
+    <p>
+    Hi! My name is Jihaan. I work in Digital Marketing in the retail space and I'm currently pursuing my Masters in Information Systems Management.  I'm hoping this will help be with the technical side of my job. 
+    </p>  
+       
+    <?php include
+    $num = Rand (1,5);
+    switch ($num)
+    {
+    case 1:
+    echo "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live";
+    break;
+    case 2:
+    echo "Programs must be written for people to read, and only incidentally for machines to execute.";
+    break;
+    case 3:
+    echo "Give a man a program, frustrate him for a day.
+    Teach a man to program, frustrate him for a lifetime.";
+    break;
+    case 4:
+    echo "Walking on water and developing software from a specification are easy if both are frozen.";
+    break;
+    case 5:
+    echo "The most important property of a program is whether it accomplishes the intention of its user.";
+    }
+    ?>
+   
+    </body>
+    </html>
